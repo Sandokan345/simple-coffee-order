905587c78a2a48e41fb94465f128341e24ab3a4a
Ahmet Yakup Çetinkaya <ahmetyakup.cetinkaya@smartiq.io>
2023-05-25 22:44:36 +0300

    created spring boot project with necessary dependencies.

---
+++ simple-coffee-order/src/main/java/com/ayc/simplecoffeeorder/SimpleCoffeeOrderApplication.java
@@ -0,0 +1,13 @@
+package com.ayc.simplecoffeeorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCoffeeOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCoffeeOrderApplication.class, args);
+	}
+
+}
